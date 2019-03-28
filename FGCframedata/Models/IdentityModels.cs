23c788d5a1e69587ca1ad9dfef1ea3f7f53d1c35
Zahari Vasilev <z.i.vasilev@gmail.com>
2019-03-29 00:45:23 +0200

    Added Route config.

--- FGCframedata/Models/IdentityModels.cs
+++ FGCframedata/Models/IdentityModels.cs
@@ -20,6 +20,10 @@ namespace FGCframedata.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Move> Moves { get; set; }
+
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
